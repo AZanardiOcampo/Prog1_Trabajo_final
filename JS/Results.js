9a6488748a6430beea408da049033c933146bca0
Felipe Alonso <alonsolareoj@udesa.edu.ar>
2023-06-12 17:46:53 -0300

    buscador

--- JS/Results.js
+++ JS/Results.js
@@ -2,14 +2,33 @@ let querystring = location.search
 let qsToObject = new URLSearchParams(querystring)
 let datoABuscar = qsToObject.get("buscar")
 
-let endpoint = `https://api.deezer.com/search?q=${datoABuscar}`
+let busqueda = document.querySelector("h1")
+busqueda.innerHTML += `<h1>Resultados de busqueda para <i>${datoABuscar}</i></h1>`
+
+let endpoint = `https://api.deezer.com/search?buscar=${datoABuscar}`
 
 fetch(endpoint)
     .then(function(response){
         return response.json()
     })
     .then(function(datos){
-        console.log(datos)
+        console.log(datos);
+        let resultados = document.querySelector("h1");
+        let contenidobuscado = document.querySelector("section");
+
+        if (datos.results.length==0){
+            resultados.innerText = `No hubo resultados para su busqueda`
+        } 
+
+        else{
+            for(i=0; i<5; i++) {
+                contenidobuscado.innerHTML += 
+                `
+                
+                `
+        }}
+
+
     })
     .catch(function(e){
         console.log(e)
