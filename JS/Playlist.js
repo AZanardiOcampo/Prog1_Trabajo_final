d018874d4c2b48a20dda084503b3748db39f6630
MateLavan <lavandeiram@udesa.edu.ar>
2023-06-16 15:39:39 -0300

    favs

--- JS/Playlist.js
+++ JS/Playlist.js
@@ -1,26 +1,24 @@
-fetch(urlcanciones)
-    .then(function(response)
-    {
-      return response.json()  
+let recuperoStorage = localStorage.getItem("Favoritos");
+let storageToArray = JSON.parse(recuperoStorage);
+apiurl = `https://api.allorigins.win/raw?url=https://api.deezer.com/track/`
+let songs = ""
+for (let i = 0; i < storageToArray.length; i++) {
+    fetch(apiurl + storageToArray[i]).then(function (response) {
+        console.log (response.json())
+        return response.json();
+    }).then(function (response){
+        songs += `<ul class="cancion"><a href="DetailsTracks.html?id=${response.id}">
+      <div class="portadas"><img class="imgportadas" src="${response.album.cover}" alt=""></div>
+          <li><p class="ajuste">${response.title}</p></li>
+          <li><p class="ajuste">${response.artist.name}</p></li></a>
+      </ul>`
+
     })
-    .then(function(data)
-    {
-        console.log(data);
-        let canciones = document.querySelector(".canciones")
-        let songs = ""
-        
-        for (let i=0; i<6; i++) 
-        {
-            songs +=`<ul class="cancion"><a href="DetailsTracks.html?id=${data.data[i].id}">
-            <div class="portadas"><img class="imgportadas" src="${data.data[i].album.cover}" alt=""></div>
-                <li><p class="ajuste">${data.data[i].title}</p></li>
-                <li><p class="ajuste">${data.data[i].artist.name}</p></li></a>
-            </ul>`
-        }
-        
-        canciones.innerHTML+= songs
-    })
-    .catch(function(error)
-    {
-        console.log("El error es: " + error);
-    })
+}
+
+
+
+
+
+canciones=document.querySelector('.cancionesFav')
+canciones.innerHTML += songs
