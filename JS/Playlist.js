a8891cc7bf5d38020556273317614146071345dd
MateLavan <lavandeiram@udesa.edu.ar>
2023-06-16 15:54:18 -0300

    modified:   CSS/styles.css 	modified:   JS/Playlist.js

--- JS/Playlist.js
+++ JS/Playlist.js
@@ -1,13 +1,14 @@
 let recuperoStorage = localStorage.getItem("Favoritos");
 let storageToArray = JSON.parse(recuperoStorage);
-apiurl = `https://api.allorigins.win/raw?url=https://api.deezer.com/track/`
-let songs = ""
+
+let apiurl = `https://api.allorigins.win/raw?url=https://api.deezer.com/track/`
+let canciones = document.querySelector('.cancionesFav')
+
 for (let i = 0; i < storageToArray.length; i++) {
     fetch(apiurl + storageToArray[i]).then(function (response) {
-        console.log (response.json())
         return response.json();
     }).then(function (response){
-        songs += `<ul class="cancion"><a href="DetailsTracks.html?id=${response.id}">
+        canciones.innerHTML += `<ul class="cancion"><a href="DetailsTracks.html?id=${response.id}">
       <div class="portadas"><img class="imgportadas" src="${response.album.cover}" alt=""></div>
           <li><p class="ajuste">${response.title}</p></li>
           <li><p class="ajuste">${response.artist.name}</p></li></a>
@@ -19,6 +20,3 @@ for (let i = 0; i < storageToArray.length; i++) {
 
 
 
-
-canciones=document.querySelector('.cancionesFav')
-canciones.innerHTML += songs
